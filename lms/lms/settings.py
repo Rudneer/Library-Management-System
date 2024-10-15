7e3adc644f5a92aec8a0b3b9bf08d28255a01dfc
unknown <neer8669@gmail.com>
2024-10-15 13:14:01 +0530

    debug true

--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$lnbnzfe3i4f8i1t_r_g((al#jw^qd97xjvu9o9jnz_0!lf)$!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
